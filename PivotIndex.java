d3fc5503c9ef2f5a17ddcf3d123490a81234d922
Mohamed-Razan <mhdrazan7@gmail.com>
2022-08-28 14:11:16 +0530

    added

--- PivotIndex.java
+++ PivotIndex.java
@@ -1,6 +1,8 @@
 package com.leetcode;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class PivotIndex {
 
@@ -18,9 +20,36 @@ public class PivotIndex {
 //        for(i = 0; i < n; i++){
 //            System.out.println(result[i]);
 //        }
+        System.out.println(pivotIndex(input));
     }
 
     public static int pivotIndex(int[] nums) {
 
+        int i;
+
+        for(i = 0; i < nums.length; i++){
+
+            Integer[] leftArray = IntStream.range(0, i)
+                    .mapToObj(j -> nums[j])
+                    .toArray(Integer[]::new);
+
+            Integer[] rightArray = IntStream.range(i+1, nums.length)
+                    .mapToObj(j -> nums[j])
+                    .toArray(Integer[]::new);
+
+            if(sumOfArray(leftArray) == sumOfArray(rightArray)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int sumOfArray(Integer[] arr){
+        int i, sum = 0;
+
+        for(i = 0; i < arr.length; i++){
+            sum = sum + arr[i];
+        }
+        return sum;
     }
 }
